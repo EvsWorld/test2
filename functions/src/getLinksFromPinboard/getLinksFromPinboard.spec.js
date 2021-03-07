23917330863fb31915e88987adb2921f8c3ab284
evsworld <evanlhendrix@gmail.com>
2021-03-07 11:48:00 +0100

    imp: getLinksFromPinboard function fetching from pinboard and batch saving to firestore

---
+++ functions/src/getLinksFromPinboard/getLinksFromPinboard.spec.js
@@ -0,0 +1,17 @@
+import getLinksFromPinboardUnwrapped from './index'
+
+import functionsTestLib from 'firebase-functions-test'
+
+const functionsTest = functionsTestLib()
+const getLinksFromPinboard = functionsTest.wrap(getLinksFromPinboardUnwrapped)
+
+describe('getLinksFromPinboard PubSub Cloud Function (PubSub:onRun)', () => {
+  // after(async () => {
+  //   functionsTest.cleanup()
+  // })
+
+  it('should handle event', async () => {
+    const results = await getLinksFromPinboard({})
+    expect(results).to.be.null
+  })
+})
---
+++ functions/src/getLinksFromPinboard/index.js
@@ -0,0 +1,86 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import axios from 'axios'
+const db = admin.firestore()
+
+const pinboardSecret = ''
+const pinboardUsername = 'EHendrix'
+const dummyLinks = [
+  {
+    u: 'https://react-hook-form.com/',
+    d: 'Home | React Hook Form - Simple React forms validation',
+    n: '',
+    dt: '2021-03-04T09:49:02Z',
+    a: 'EHendrix',
+    t: ['tom', 'mike']
+  },
+  {
+    u: 'https://www.thepullrequest.com/p/the-american-dream-as-a-service',
+    d: 'The American-Dream-as-a-Service - The Pull Request',
+    n: null,
+    dt: '2021-03-04T01:31:08Z',
+    a: 'EHendrix',
+    t: ['dan', 'mike', 'puzzle-pieces', 'tom']
+  },
+  {
+    u: 'https://a16z.com/2021/02/27/nfts-and-a-thousand-true-fans/',
+    d: 'a16z.com',
+    n: '',
+    dt: '2021-03-04T00:52:02Z',
+    a: 'EHendrix',
+    t: ['mike', 'tom']
+  }
+]
+/**
+ * Handle getLinksFromPinboard pub sub event
+ * @param {functions.pubsub.Context} context - Function context
+ * @returns {Promise}
+ */
+async function getLinksFromPinboardEvent(context) {
+  console.log('Pub Sub message: ', { context })
+  const batch = db.batch()
+
+  const pinboardSecret = functions.config().pinboard.secret
+  const pinboardUsername = functions.config().pinboard.username
+
+  const linksResults = await axios.get(
+    `http://feeds.pinboard.in/json/secret:${pinboardSecret}/u:${pinboardUsername}/t:tom`
+  )
+  const links = linksResults.data
+  console.log('links:>> ', links)
+
+  // writes
+  links.forEach((link) => {
+    const linkRef = admin.firestore().collection('links').doc()
+    batch.set(linkRef, link)
+  })
+
+  batch.commit()
+
+  // End function execution by returning
+  return null
+}
+// TODO: fetch all documents from pinboard
+
+// TODO: get ref to where we want to save them (/users/{userId})
+
+// TODO: function to loop over all the users in db,
+
+// call pinboard to get all their new links,
+
+// and then save those links in that user's 'backlog' array field
+
+// const schedule = 'every 1 minutes'
+const schedule = '* * * * *'
+
+/**
+ * Cloud Function triggered on a specified CRON schedule
+ *
+ * Trigger: `PubSub - onRun`
+ *
+ * @name getLinksFromPinboard
+ * @type {functions.CloudFunction}
+ */
+export default functions.pubsub
+  .schedule(schedule)
+  .onRun(getLinksFromPinboardEvent)
