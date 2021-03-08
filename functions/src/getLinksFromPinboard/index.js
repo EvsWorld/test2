3e6867ddfd572cc11ab95ccc813f6da972a70c4e
evsworld <evanlhendrix@gmail.com>
2021-03-08 13:35:55 +0100

    refactor: break into functions. However now 'parents' tag is saving duplicates

--- functions/src/getLinksFromPinboard/index.js
+++ functions/src/getLinksFromPinboard/index.js
@@ -38,89 +38,98 @@ export const collectIdsAndDocs = (doc) => {
 /**
  * Handle getLinksFromPinboard pub sub event
  * @param {functions.pubsub.Context} context - Function context
- * @returns {Promise}
+ * @returns {Promise} Returns null
  */
 async function getLinksFromPinboardEvent(context) {
   // console.log('Pub Sub message: ', { context })
 
-  // TODO: loop over all users
+  loopOverUsers(saveLinks)
+
+  // TODO:function that runs and checks if there is no link in user’s
+  // ‘sharedLinks’ that was created today. If not, then it will look in that
+  // user’s ‘backlog’ collection  for the link with the oldest link, copy it to
+  // the ‘sharedLinks’ collection and then delete it from backlog, then  and
+  // sends message
+
+  // TODO:  send user a message with their url to their custom domain of my site, and
+  // change the metadata to that url (done either something on the message or
+  // the url itself)
+  // function sendMessage( ) { }
+
+  // End function execution by returning
+  return null
+
   /**
-   *
+   * Loop over all users fetching and saving links
    */
-  async function loopOverUsers() {
+  async function loopOverUsers(cb) {
     const snapshot = await admin.firestore().collection('users').get()
     const users = snapshot.docs.map(collectIdsAndDocs)
     // console.log('users :>> ', users)
-    users.forEach(saveLinks)
+    users.forEach(cb)
   }
-  /**
-   * @param user
-   */
-  loopOverUsers()
 
   /**
-   * @param userRef
-   * @param user
+   * @param {object} user - individual user object from firebase, with Id added
+   * @returns {Promise} - Resolves with links from pinboard for specified user
    */
   async function fetchLinks(user) {
     //  fetch all documents from pinboard
     //  TODO: put this in function getLinksFromPinboard(user)
     const pinboardSecret = functions.config().pinboard.secret
     const pinboardUsername = functions.config().pinboard.username
-    console.log(
-      'axios url :>> ',
-      `http://feeds.pinboard.in/json/secret:${pinboardSecret}/u:${pinboardUsername}/t:${user.username}`
-    )
     const linksResults = await axios.get(
-      // `http://feeds.pinboard.in/json/secret:${pinboardSecret}/u:${pinboardUsername}/t:tom`
       `http://feeds.pinboard.in/json/secret:${pinboardSecret}/u:${pinboardUsername}/t:${user.username}`
     )
     const links = linksResults.data
     return links
   }
 
-  //  TODO: put this in function saveLinksToUser(user)
-  //  saveLinksToUser(user) {
-  // const userlinks = getLinksFromPinboard(user)
-  // }
   /**
-   * @param user
+   * Fetch links and save is user's backlog
+   * @param {object} user - individual user object from firebase, with Id added
    */
   async function saveLinks(user) {
     const links = await fetchLinks(user)
     links.forEach(async (linkData) => {
-      console.log({ id: user.id, username: user.username })
-      const userBacklog = admin
-        .firestore()
-        .collection('users')
-        .doc(user.id)
-        .collection('backlog')
+      // console.log({ id: user.id, username: user.username })
+      const userBacklog = getCollection(user, 'backlog')
 
-      const linkSnapshot = await userBacklog.where('u', '==', linkData.u).get()
-      if (linkSnapshot.empty) {
-        console.log('No matching documents')
-        try {
-          userBacklog.add(linkData)
-        } catch (error) {
-          console.error('error adding link to user backlog')
-        }
-      }
+      await upsertLink(userBacklog, linkData)
     })
   }
-  // TODO: function sendMessage( ) {
-  // send user a message with their url to their custom domain of my site, and
-  // change the metadata to that url (done either something on the message or
-  // the url itself)
 
-  // }
+  /**
+   * Check if a link with that url already exists, if not then add it
+   * @param {admin.firstore.CollectionReference} collection - User backlog collection reference
+   * @param {object} linkData - link data to insert from pinboard
+   */
+  async function upsertLink(collection, linkData) {
+    const linkSnapshot = await collection.where('u', '==', linkData.u).get()
+    if (linkSnapshot.empty) {
+      console.log('No matching documents')
+      try {
+        collection.add(linkData)
+      } catch (error) {
+        console.error('error adding link to user backlog')
+      }
+    }
+  }
 
-  // End function execution by returning
-  return null
+  /**
+   * @param {object} user - individual user object from firebase, with Id added
+   * @param collection
+   * @returns {Promise} Resolves with sub collectionReference one level down from specified user
+   */
+  function getCollection(user, collection) {
+    return admin
+      .firestore()
+      .collection('users')
+      .doc(user.id)
+      .collection(collection)
+  }
 }
 
-// call pinboard to get all their new links,
-
-// and then save those links in that user's 'backlog' array field
 
 // const schedule = 'every 1 minutes'
 const schedule = '* * * * *'
