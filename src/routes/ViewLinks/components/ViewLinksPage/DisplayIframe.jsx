4a1ec0a2231a715bbb3791d7cc23e14e8248304a
evsworld <evanlhendrix@gmail.com>
2021-03-22 13:48:59 +0100

    imp: iframe preview page with iframely

---
+++ src/routes/ViewLinks/components/ViewLinksPage/DisplayIframe.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export function DisplayIframe({ topLink }) {
+  const [iframeHtml, setIframeHtml] = useState(null)
+
+  // TODO: fetch iframely html
+  const apiKey = process.env.REACT_APP_IFRAMELY_API_KEY
+  useEffect(() => {
+    const config = {
+      method: 'get',
+      url: `https://iframe.ly/api/oembed?url=${topLink}&api_key=${apiKey}&iframe=1&omit_script=1`,
+      headers: {
+        authority: 'iframe.ly',
+        'sec-ch-ua':
+          '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        dnt: '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        referer: 'https://iframely.com/',
+        'accept-language':
+          'en-US,en;q=0.9,es;q=0.8,es-419;q=0.7,ca;q=0.6,it;q=0.5'
+      }
+    }
+
+    axios(config)
+      .then(function (response) {
+        const data = response.data
+        setIframeHtml(data.html)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }, [])
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: iframeHtml
+      }}
+    />
+  )
+}
+
+DisplayIframe.propTypes = {
+  topLink: PropTypes.string.isRequired
+}
--- src/routes/ViewLinks/components/ViewLinksPage/ViewLinksPage.jsx
+++ src/routes/ViewLinks/components/ViewLinksPage/ViewLinksPage.jsx
@@ -1,67 +1,11 @@
-import PropTypes from 'prop-types'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { useFirestore, useUser, useFirestoreCollectionData } from 'reactfire'
-import axios from 'axios'
 // import ProjectTile from '../ProjectTile'
 import styles from './ViewLinksPage.styles'
+import { DisplayIframe } from './DisplayIframe'
+import { USERS_COLLECTION } from 'constants/firebasePaths'
 const useStyles = makeStyles(styles)
-
-export function DisplayIframe({ topLink }) {
-  const [iframeHtml, setIframeHtml] = useState(null)
-  // TODO: fetch iframely html
-
-  console.log('topLink :>> ', topLink)
-
-  const apiKey = process.env.REACT_APP_API_KEY
-  useEffect(() => {
-    const config = {
-      method: 'get',
-      url: `https://iframe.ly/api/oembed?url=${topLink}&api_key=${apiKey}&iframe=1&omit_script=1`,
-      headers: {
-        authority: 'iframe.ly',
-        'sec-ch-ua':
-          '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        dnt: '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-        accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'cross-site',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        referer: 'https://iframely.com/',
-        'accept-language':
-          'en-US,en;q=0.9,es;q=0.8,es-419;q=0.7,ca;q=0.6,it;q=0.5'
-      }
-    }
-
-    axios(config)
-      .then(function (response) {
-        const data = response.data
-        console.log(JSON.stringify(data.html))
-        setIframeHtml(data.html)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  }, [])
-  // return dangerously set html
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: iframeHtml
-      }}
-    />
-  )
-}
-
-DisplayIframe.propTypes = {
-  topLink: PropTypes.string.isRequired
-}
 
 function useLinksList() {
   // const sharedLinksCollection = await getCollection(user, 'sharedLinks')
@@ -72,7 +16,7 @@ function useLinksList() {
   const firestore = useFirestore()
 
   const linksRef = firestore
-    .collection('users')
+    .collection(USERS_COLLECTION)
     .doc(auth.uid)
     .collection('sharedLinks')
 
@@ -87,24 +31,22 @@ function useLinksList() {
 
 function ViewLinksPage() {
   const classes = useStyles()
-
   const { links } = useLinksList()
-  const topLink = links && links[0]?.u
-  console.log('topLink :>> ', typeof topLink)
+  const topLinka = links.shift().u
+  const restOfLinks = links
 
   return (
     <div className={classes.root}>
       <span>ViewLinks Component</span>
 
       <div className={classes.tiles}>
-        <DisplayIframe topLink={topLink} />
-        {links &&
-          links.map((link, ind) => {
+        <DisplayIframe topLink={topLinka} />
+        {restOfLinks &&
+          restOfLinks.map((link, ind) => {
             return (
               <div key={`Link-${link.uid}-${ind}`}>
-                <a rel="noopener noreferrer" target="_blank" href={link.u}>
-                  {link.u}
-                </a>
+                <DisplayIframe topLink={link.u} />
+                <br />
               </div>
             )
           })}
